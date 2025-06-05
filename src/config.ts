9627af0e9e11abb94f5bc3e7f42fe66eb879032a
AyushmanTripathy <ayushmantripathy2004@gmail.com>
2025-06-05 12:34:13 +0530

    feat: PaymentMethodService

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,12 @@
 export const PAGE_SIZE = 20
-export const REGION_ID =  'reg_01JWBSQY5SZ8G7DHBG2GPCPEVA'
+export const REGION_ID = 'reg_01JWBSQY5SZ8G7DHBG2GPCPEVA'
+
+export const paymentMethodFromId = {
+  'pp_system_default': {
+    name: 'COD',
+    description: "Cash on delivery, manual payments.",
+    apiKey: '',
+    isTest: true,
+    img: '/static/payment/cod.png'
+  }
+}
